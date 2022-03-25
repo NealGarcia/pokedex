3fe62d3e8242389368bc9c7d23f71c9eab5fdb61
nealgarcia <nathnlgarcia@gmail.com>
2022-03-25 12:10:43 -0500

    added useStyles to display cardMedia

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,14 +1,23 @@
-import { Card, Grid, CardMedia, CardContent, Typography } from '@material-ui/core';
+import { Card, Grid, CardMedia, CardContent, Typography, makeStyles } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles((theme) => ({
+    cardMedia: {
+        margin: "auto",
+        width: 130,
+        height: 130
+    }
+}))
+
 function PokemonCard(props) {
+    const classes = useStyles()
     const { pokemon, image } = props
     const { id, name } = pokemon
     return (
         // xs small devices take up 12 slots; sm devices take up 2 slots 
         <Grid item xs ={12} sm ={2}> 
             <Card>
-                <CardMedia image = {image}></CardMedia>
+                <CardMedia className = {classes.cardMedia} image = {image}></CardMedia>
                 <CardContent>
                     <Typography>
                         {name}
--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
 export const POKEMON_API_URL = "https://pokeapi.co/api/v2/pokemon"
-export const IMG_API_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon"
+export const IMG_API_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
