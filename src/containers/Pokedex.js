ec06a411d483a3bf7f66cfa1019db61992ebd71a
nealgarcia <nathnlgarcia@gmail.com>
2022-03-25 10:58:40 -0500

    create config folder to hold api url

--- src/containers/Pokedex.js
+++ src/containers/Pokedex.js
@@ -2,11 +2,12 @@ import { Box } from '@material-ui/core';
 import { useState, useEffect } from 'react';
 import React from 'react';
 import axios from 'axios';
+import { POKEMON_API_URL } from '../config';
 
 function Pokedex(props) {
     // Fetch data from API
     useEffect(()=> {
-        axios.get("https://pokeapi.co/api/v2/pokemon?limit=800")
+        axios.get(`${POKEMON_API_URL}?limit=800`)
         .then((response) => {
             console.log(response.data)
         })
