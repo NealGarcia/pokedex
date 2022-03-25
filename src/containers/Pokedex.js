6b7261d539461199ce2028ec3a86f4a59466124d
nealgarcia <nathnlgarcia@gmail.com>
2022-03-25 10:55:34 -0500

    fetch data from pokeAPI and consolelog

--- src/containers/Pokedex.js
+++ src/containers/Pokedex.js
@@ -1,9 +1,22 @@
+import { Box } from '@material-ui/core';
+import { useState, useEffect } from 'react';
 import React from 'react';
+import axios from 'axios';
 
 function Pokedex(props) {
+    // Fetch data from API
+    useEffect(()=> {
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=800")
+        .then((response) => {
+            console.log(response.data)
+        })
+    }, [])
+
     return (
         <div>
-            Test
+            <Box>
+
+            </Box>
         </div>
     );
 }
