79135f793e8b1cc6433779806da815bd60d0ece8
nealgarcia <nathnlgarcia@gmail.com>
2022-03-27 12:58:35 -0500

    styling and layout to pokemon details container

--- src/containers/PokemonDetails.js
+++ src/containers/PokemonDetails.js
@@ -1,6 +1,23 @@
+import { Box, CircularProgress, Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/styles';
 import axios from 'axios';
 import React, { Component } from 'react';
 import { POKEMON_API_URL } from '../config';
+
+const styles = (theme) => ({
+    pokedexContainer: {
+        height: '84vh',
+        backgroundColor: 'black',
+        color: 'white',
+        marginTop: '75px',
+        textAlign: 'center',
+        borderRadius: '5px',
+        paddingTop: '30px',
+    },
+    textTitle: {
+        textTransform: 'upperCase',
+    },
+})
 
 class PokemonDetails extends Component {
     constructor(props){
@@ -14,15 +31,30 @@ class PokemonDetails extends Component {
         const { match } = this.props
         const { id } = match?.params
         axios.get(POKEMON_API_URL + "/" + id)
+            .then((response) => {
+                if(response.status >= 200 && response.status < 300){
+                    this.setState({pokemon: response.data})
+                }
+            })
     }
-
     render() {
-        return (
-            <div>
-                <h1 style = {{marginTop: 200}}>Test</h1>
-            </div>
-        );
+        const { classes } = this.props
+        const { pokemon } = this.state
+        if(pokemon){
+            const { name } = pokemon
+            return(
+                <Box >
+                    <Box className = {classes.pokedexContainer}>
+                        <Typography className = {classes.textTitle} variant="h1">{name}</Typography>
+                    </Box>
+                </Box>
+            )
+        }
+        else{
+            return <CircularProgress/>
+        }
     }
 }
 
-export default PokemonDetails;
+
+export default withStyles(styles)(PokemonDetails);
